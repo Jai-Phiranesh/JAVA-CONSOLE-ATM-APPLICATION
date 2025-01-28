7009fe310dae8de0445edcf0cbfcd9d3e2c74414
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-01-28 22:58:16 +0530

    Delete ATM/Tester.java

--- ATM/Tester.java
+++
@@ -1,8 +0,0 @@
-package ATM;
-// Main class to start the ATM App
-public class Tester {
-    public static void main(String[] args) throws CloneNotSupportedException {
-        System.out.println("\n" + "\n" + "WELCOME TO THE ATM" + "\n" + "\n");
-        ATMActions.start();  // Call the start method to start ATM actions
-    }
-}
