fdc7aa5ae6907f25c56a6ac8c45ed20ab22a682f
JAIPHIRANESH <jaiphiranesh8072@gmail.com>
2025-01-28 23:00:40 +0530

    Delete ATM/Listofnotes directory

--- ATM/Listofnotes/Ru500.java
+++
@@ -1,9 +0,0 @@
-package ATM.Listofnotes;
-
-import ATM.Notes.Notes;
-
-public class Ru500 extends Notes {// Class representing 500 notes, inheriting from Notes class
-    public Ru500(long count,String name) {
-        super(name,count);
-    }
-}
